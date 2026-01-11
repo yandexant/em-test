218afab6e15f2cea2d05f6c0a139c890d231f1f4
yandexant <planeant@yandex.ru>
2026-01-11 14:33:02 +1000

    Emobile бэк и нжинкс

---
+++ backend/wserv.py
@@ -0,0 +1,17 @@
+# веб отвечалка на 8080 порту
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    server.serve_forever()
